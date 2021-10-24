90c21067aa121272b52c02266ff02b2e022d65e1
Ion Paciu <ionpaciu999@gmail.com>
2021-10-24 14:40:25 +0200

    Update bme_measurement.py

--- bme_measurement.py
+++ bme_measurement.py
@@ -1,30 +1,24 @@
 import bme280
-import i2c_pwm
+import smbus2
 import time
 
 # Stuff for BME280
 ADDRESS_BME = 0x76
-calibration_params = None
+
+
+I2C_PORT = 1
+bus = smbus2.SMBus(I2C_PORT)
+
+calibration_params = bme280.load_calibration_params(bus, ADDRESS_BME)
 
 def measure():
 	global ADDRESS_BME
 	global calibration_params
 	global TEMPERATURE_OFFSET
 
-	with i2c_pwm.i2c_lock:
-		sample = bme280.sample(i2c_pwm.i2c, ADDRESS_BME, calibration_params)
+	sample = bme280.sample(bus, ADDRESS_BME, calibration_params)
 	data = {'temperature': sample.temperature, 'pressure': sample.pressure, 'humidity': sample.humidity}
 	print(data)
-
-def setup():
-	with i2c_pwm.i2c_lock:
-		try:
-			calibration_params = bme280.load_calibration_params(i2c_pwm.i2c, ADDRESS_BME)
-		except Exception as e:
-			print(e)
-
-
-setup()
 
 for i in range(10):
 	measure()
